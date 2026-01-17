ce15e164f5f2fc9d74125185f4e7ca6df2afb55c
lokytech5 <lokytech5@gmail.com>
2026-01-17 04:37:51 +0000

    refractor by moving schema to its own file from main.py

--- app/main.py
+++ app/main.py
@@ -6,10 +6,10 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Dict, List, Optional
 
-from pydantic import BaseModel
 
 from fastapi import FastAPI, HTTPException, Body
 
+from app.api.schemas import AdzunaIngestIn, DebugAddJobIn, IngestAllIn, IngestAllOut, IngestOut, JobOut, ReedIngestIn
 import httpx
 
 from app.core.config import settings
@@ -37,53 +37,7 @@ app = FastAPI(title="Job Collector (Learning Version)")
 # -----------------------------
 # Step 3C: API schemas (Pydantic)
 # -----------------------------
-class JobOut(BaseModel):
-    uid: str
-    source: str
-    source_job_id: str
-    title: str
-    company: Optional[str] = None
-    location: Optional[str] = None
-    url: Optional[str] = None
-    posted_at: Optional[datetime] = None
 
-
-class DebugAddJobIn(BaseModel):
-    title: str
-    company: Optional[str] = None
-
-
-class IngestOut(BaseModel):
-    fetched: int
-    inserted: int
-    total_in_store: int
-
-
-class ReedIngestIn(BaseModel):
-    keywords: str
-    location_name: Optional[str] = None
-    results_to_take: int = 25
-
-
-class AdzunaIngestIn(BaseModel):
-    what: str
-    where: Optional[str] = None
-    results_per_page: int = 20
-    page: int = 1
-
-
-class IngestAllIn(BaseModel):
-    keywords: str = "backend engineer"
-    location_name: Optional[str] = "London"
-    reed_results: int = 25
-    adzuna_results: int = 20
-    adzuna_page: int = 1
-
-
-class IngestAllOut(BaseModel):
-    reed: IngestOut
-    adzuna: IngestOut
-    total_in_store: int
 
 # -----------------------------
 # Endpoints
